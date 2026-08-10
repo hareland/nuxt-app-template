9c0cc87aba945045e18d69db9f6044f8950ab4f2
hareland <packagist@proton.me>
2026-08-11 01:44:23 +0200

    seed users: add user account creation with upsert logic and logging

--- server/tasks/db/seed/users.ts
+++ server/tasks/db/seed/users.ts
@@ -1,11 +1,14 @@
 import type { UserInsert } from '@nuxthub/db/schema'
 import { createUser } from '#server/utils/user.ts'
+import { db, schema } from '@nuxthub/db'
+import { createLogger } from '#shared/utils/logger.ts'
 
 export default defineTask({
   meta: {
     name: 'db:seed:users',
   },
   async run() {
+    const logger = createLogger('db:seed:users')
     const users: UserInsert[] = [
       {
         id: 'user_seed_1',
@@ -23,9 +26,45 @@ export default defineTask({
 
     const createdUsers = await Promise.all(users.map(user => createUser(user)))
 
+    const createdAccounts = await Promise.all(createdUsers.map(async (user) => {
+      const accessToken = Date.now().toString()
+      const refreshToken = Date.now().toString()
+      const [created] = await db.insert(schema.userAccount)
+        .values({
+          id: `acc_${user.id}`,
+          userId: user.id,
+          provider: 'email',
+          providerAccountId: user.email,
+          type: 'email',
+          accessToken,
+          refreshToken,
+        }).onConflictDoUpdate({
+          target: schema.userAccount.id,
+          set: {
+            userId: user.id,
+            accessToken,
+            refreshToken,
+          },
+        }).returning()
+
+      if (!created) {
+        throw new Error(`Failed to create/upsert account for user ${user.id}[${JSON.stringify(user)}]`)
+      }
+
+      return created
+    }))
+
+    const userIdMappedAccounts = Object.fromEntries(
+      createdAccounts.map(account => [account.userId!, account]),
+    )
+
     return {
       result: {
         users: createdUsers.map((user) => {
+          if (userIdMappedAccounts[user.id]) {
+            return `${user.id}[${userIdMappedAccounts[user.id as keyof typeof userIdMappedAccounts]?.id}]`
+          }
+
           return user.id
         }),
       },
