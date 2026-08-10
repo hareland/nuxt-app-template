3107eabde8dc77e133e5a800189705d83697a67b
hareland <packagist@proton.me>
2026-08-11 01:43:32 +0200

    reorganize database schema: add `user_accounts` table, update `users` and `user_passkeys` definitions

--- server/db/migrations/sqlite/0000_simple_legion.sql
+++ server/db/migrations/sqlite/0000_simple_legion.sql
@@ -2,10 +2,10 @@ CREATE TABLE `users` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text,
 	`email` text NOT NULL,
+	`role` text DEFAULT 'user' NOT NULL,
+	`settings` text,
 	`last_login_at` integer,
 	`email_verified_at` integer,
-	`type` text DEFAULT 'user' NOT NULL,
-	`settings` text,
 	`accepted_terms_at` integer,
 	`accepted_privacy_at` integer,
 	`created_at` integer NOT NULL,
@@ -13,6 +13,22 @@ CREATE TABLE `users` (
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);--> statement-breakpoint
+CREATE TABLE `user_accounts` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`provider` text NOT NULL,
+	`provider_account_id` text NOT NULL,
+	`type` text NOT NULL,
+	`scope` text,
+	`access_token` text,
+	`refresh_token` text,
+	`expires_at` integer,
+	`token_type` text,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
 CREATE TABLE `user_passkeys` (
 	`id` text PRIMARY KEY NOT NULL,
 	`user_id` text NOT NULL,
--- server/db/schema.sqlite.ts
+++ server/db/schema.sqlite.ts
@@ -1,9 +1,10 @@
 //
-import { index, integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { index, integer, sqliteTable, text, customType } from 'drizzle-orm/sqlite-core'
 import { relations } from 'drizzle-orm'
 import type { UserRole } from '#shared/schema'
 import { userRoles } from '#shared/schema'
 import { createDomainId } from '#shared/utils'
+import { decryptSync, encryptSync } from '#server/utils/crypto'
 
 const withTimestamps = {
   createdAt: integer({ mode: 'timestamp' }).notNull().$defaultFn(() => new Date()),
@@ -17,33 +18,76 @@ const domainId = (prefix?: string) => {
   return text().primaryKey().$defaultFn(() => createDomainId(prefix))
 }
 
+export function encrypted<TData extends Record<string, unknown> | string>() {
+  return customType<{ data: TData, driverData: string }>({
+    dataType() {
+      return 'text'
+    },
+    toDriver(value) {
+      return encryptSync(value)
+    },
+    fromDriver(value) {
+      return decryptSync<TData>(value)
+    },
+  })
+}
+
 export const user = sqliteTable('users', {
   id: domainId('user_'),
-  name: text('name'),
-  email: text('email').notNull().unique(),
-  role: text('type', { enum: userRoles }).notNull().$type<UserRole>().default('user'),
-  settings: text('settings', { mode: 'json' }).$type<{
+  name: text(),
+  email: text().notNull().unique(),
+  role: text({ enum: userRoles }).notNull().$type<UserRole>().default('user'),
+  settings: text({ mode: 'json' }).$type<{
     locale?: string | null
     currency?: string | null
   }>(),
-  lastLoginAt: integer('last_login_at', { mode: 'timestamp' }),
-  emailVerifiedAt: integer('email_verified_at', { mode: 'timestamp' }),
-  acceptedTermsAt: integer('accepted_terms_at', { mode: 'timestamp' }),
-  acceptedPrivacyAt: integer('accepted_privacy_at', { mode: 'timestamp' }),
+  lastLoginAt: integer({ mode: 'timestamp' }),
+  emailVerifiedAt: integer({ mode: 'timestamp' }),
+  acceptedTermsAt: integer({ mode: 'timestamp' }),
+  acceptedPrivacyAt: integer({ mode: 'timestamp' }),
   ...withTimestamps,
 })
 
 export const userPasskey = sqliteTable('user_passkeys', {
-  id: text('id').primaryKey(),
-  userId: text('user_id').notNull().references(() => user.id, { onDelete: 'cascade' }),
-  credential: text('credential', { mode: 'json' }).$type<Record<string, unknown>>().notNull(),
+  id: domainId('passkey_'),
+  userId: text().notNull().references(() => user.id, { onDelete: 'cascade' }),
+  credential: text({ mode: 'json' }).$type<Record<string, unknown>>().notNull(),
   ...withTimestamps,
 }, t => [
   index('user_passkey_user_idx').on(t.userId),
 ])
 
+export const userAccount = sqliteTable('user_accounts', {
+  id: domainId('account_'),
+  userId: text().notNull().references(() => user.id, { onDelete: 'cascade' }),
+  provider: text().notNull(),
+  providerAccountId: text().notNull(),
+  type: text().notNull(),
+  scope: text(),
+  accessToken: text(),
+  refreshToken: text(),
+  expiresAt: integer({ mode: 'timestamp' }),
+  tokenType: text('token_type'),
+  ...withTimestamps,
+})
+
 export const userRelations = relations(user, ({ many }) => ({
   passkeys: many(userPasskey),
+  accounts: many(userAccount),
+}))
+
+export const userPasskeyRelations = relations(userPasskey, ({ one }) => ({
+  user: one(user, {
+    fields: [userPasskey.userId],
+    references: [user.id],
+  }),
+}))
+
+export const userAccountRelations = relations(userAccount, ({ one }) => ({
+  user: one(user, {
+    fields: [userAccount.userId],
+    references: [user.id],
+  }),
 }))
 
 // types
@@ -51,3 +95,5 @@ export type UserInsert = typeof user.$inferInsert
 export type UserSelect = typeof user.$inferSelect
 export type UserPasskeyInsert = typeof userPasskey.$inferInsert
 export type UserPasskeySelect = typeof userPasskey.$inferSelect
+export type UserAccountInsert = typeof userAccount.$inferInsert
+export type UserAccountSelect = typeof userAccount.$inferSelect
